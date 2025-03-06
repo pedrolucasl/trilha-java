0cd850d6d72ad6f4d7cc9385fbe9c4b608034632
Pedro Lucas <plucaslopes391@gmail.com>
2025-03-05 22:02:48 -0300

    MODIFICAÇÕES: Método criado para imprimir os os candidatos selecionados atravez do 'FOR'

--- controle-candidatura/src/ProcessoSeletivo.java
+++ controle-candidatura/src/ProcessoSeletivo.java
@@ -3,17 +3,27 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
        
-      selecaoCandidatos();
+     imprimirSelecionados();
 
+    }
+
+    static void imprimirSelecionados(){
+      String [] candidatos = {"FELIPE","MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+      System.out.println("Imprimindo a lista de candidatos junto com o seu índice");
+
+      for(int indice = 0; indice < candidatos.length; indice++){
+          System.out.println("O candidato de n° " +(indice+1)+ " é " +candidatos[indice]); /*INDICE+1 FAZ QUE ELE NÃO COMECE NO VALOR 0  */
+      }
+      
     }
       static void selecaoCandidatos(){
         String [] candidatos = {"FELIPE","MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
 
         int candidatosSelecionados = 0; /*CONTADOR DE CADIDATOS */
         int candidatoAtual = 0; /*INDICE DO ARRAY */
-        double salarioBase = 2200.0;
+        double salarioBase = 2000.0;
 
-        while (candidatosSelecionados < 5) { /*ENQUANTO A SELEÇÃO FOR MENOR QUE 5, ELE IRÁ RODAR O WHILE */
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) { /*ENQUANTO A SELEÇÃO FOR MENOR QUE 5, ELE IRÁ RODAR O WHILE */
           String candidato = candidatos[candidatoAtual]; /*VARIVAEL STRING (CANDIDATO) RECEBE O INDICE DO ARRAY (POSIÇÃO ATUAL) */
           double salarioPretendido = valorPretendido();
 
